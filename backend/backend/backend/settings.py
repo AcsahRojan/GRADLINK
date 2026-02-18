0a16ba12e069b81128039901f45a84df84b1c9b6
Acsah Rojan <acsah.rojan@gmail.com>
2026-02-18 13:32:59 +0530

    sensitive info hidden

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gradlinkdb',
-        'USER': 'root',
-        'PASSWORD': 'roots',
-        'HOST': 'localhost',
-        'PORT': '3307',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '3307'),
     }
 }
 
